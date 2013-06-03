0efe936127e85e5c1cfef39a197678c036f84928
Nicolas McCurdy <thenickperson@gmail.com>
2013-06-02 20:37:59 -0400

    Add some basic logic to the Location model

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,23 @@
 class Location < ActiveRecord::Base
+
   attr_accessible :name, :weekday_end, :weekday_start, :weekend_end, :weekend_start
+
+  def open? time=Time.now
+    if is_weekday? time
+      @weekday_hours.cover? time
+    else
+      @weekend_hours.cover? time
+    end
+  end
+
+  def <=> other_location
+    @name <=> other_location.name
+  end
+
+  private
+
+  def is_weekday? time
+    not (time.saturday? || time.sunday?)
+  end
+
 end
