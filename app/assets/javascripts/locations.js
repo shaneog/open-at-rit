a3f06c93e6a600853af373359d64c400093f5db1
Nicolas McCurdy <thenickperson@gmail.com>
2013-06-05 23:30:43 -0400

    Sync the heights of the locations in the view (via JS) when the page is loaded

--- app/assets/javascripts/locations.js
+++ app/assets/javascripts/locations.js
@@ -1,2 +1,12 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+var Locations = {
+  syncHeights: function () {
+    $('.location').css('height', $('.location').height());
+  }
+};
+
+$(function () {
+  Locations.syncHeights();
+});
