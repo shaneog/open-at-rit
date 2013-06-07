2fcdaedcd1f4521825df17e0ff0724011fbe8398
Nicolas McCurdy <thenickperson@gmail.com>
2013-06-06 22:19:09 -0400

    Add a LOCATIONS_PER_ROW setting to the LocationsHelper and use it
    This makes it easier to change the number of locations per row on the fly, since
    the helper can calculate location width instead of the views.

--- app/helpers/locations_helper.rb
+++ app/helpers/locations_helper.rb
@@ -1,2 +1,11 @@
 module LocationsHelper
+
+  # The number of locations to show per row (for screens with enough room).
+  LOCATIONS_PER_ROW = 4
+
+  # The relative width of a location (assuming Bootstrap's default grid system
+  # with 12 units per row). Note that the width of each location is rounded down
+  # if necessary.
+  LOCATION_WIDTH = 12 / LOCATIONS_PER_ROW
+
 end
