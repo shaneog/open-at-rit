7c6063ebf21984b322fab9a64fab67493cd1a1a8
Nicolas McCurdy <thenickperson@gmail.com>
2013-06-08 17:09:47 -0400

    Add a few more tests to LocationTest

--- test/models/location_test.rb
+++ test/models/location_test.rb
@@ -1,7 +1,59 @@
 require 'test_helper'
+require 'chronic'
 
 class LocationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do
+    @corner_store = locations :corner_store
+  end
+
+  #test 'should have accessible attributes'
+
+  #test 'should be sorted by name'
+
+  #test 'should validate its name'
+
+  test 'should know if it is open at a certain time' do
+    assert @corner_store.open?(Chronic.parse '8 am on Monday')
+    assert @corner_store.open?(Chronic.parse '12 pm on Monday')
+    assert !@corner_store.open?(Chronic.parse '2 am on Tuesday')
+
+    assert @corner_store.open?(Chronic.parse '10:30 am on Saturday')
+    assert @corner_store.open?(Chronic.parse '12 pm on Saturday')
+    assert !@corner_store.open?(Chronic.parse '2 am on Sunday')
+  end
+
+  test 'should get its hours during the week' do
+    assert_equal @corner_store.weekday_hours, '8:00 am to 2:00 am'
+  end
+
+  test 'should get its hours during the weekend' do
+    assert_equal @corner_store.weekend_hours, '10:30 am to 2:00 am'
+  end
+
+  test 'should compare itself (by name) with another location' do
+    assert_equal self <=> Location.new(name: 'a'), -1
+    assert_equal self <=> Location.new(name: 'The Corner Store'), 0
+    assert_equal self <=> Location.new(name: 'z'), 1
+  end
+
+  test 'should know if it is open during the week' do
+    assert @corner_store.open_weekdays?
+  end
+
+  test 'should know if it is open during the weekend' do
+    assert @corner_store.open_weekdays?
+  end
+
+  test 'should know if a certain time is a weekday' do
+    assert @corner_store.is_weekday?(Chronic.parse 'Monday')
+    assert @corner_store.is_weekday?(Chronic.parse 'Tuesday')
+    assert @corner_store.is_weekday?(Chronic.parse 'Wednesday')
+    assert @corner_store.is_weekday?(Chronic.parse 'Thurdsday')
+    assert @corner_store.is_weekday?(Chronic.parse 'Friday')
+
+    assert !@corner_store.is_weekday?(Chronic.parse 'Saturday')
+    assert !@corner_store.is_weekday?(Chronic.parse 'Sunday')
+  end
+
 end
