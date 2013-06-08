632ff62a5978ef38a993d9a6dc9e591b0d8f0d9e
Nicolas McCurdy <thenickperson@gmail.com>
2013-06-08 15:44:19 -0400

    Add tests for the constants in the LocationsHelper

--- test/helpers/locations_helper_test.rb
+++ test/helpers/locations_helper_test.rb
@@ -1,4 +1,13 @@
 require 'test_helper'
 
 class LocationsHelperTest < ActionView::TestCase
+
+  test 'should get the number of locations per row' do
+    assert_equal 4, LocationsHelper::LOCATIONS_PER_ROW
+  end
+
+  test 'should get the width of each location' do
+    assert_equal 3, LocationsHelper::LOCATION_WIDTH
+  end
+
 end
