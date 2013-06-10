fedc2aabfd062fe832b2c31d184d7853c43ef9c8
Nicolas McCurdy <thenickperson@gmail.com>
2013-06-09 22:31:30 -0400

    Fix a couple issues with Location#open? where times weren't adjusted properly
    1. The time from the method's argument is now set to have the date January 1st,
       2000 to prevent invalid comparisons.
    2. The end times for hours are now moved a day forward if they are (initially)
       smaller than the appropriate end times.

--- app/models/location.rb
+++ app/models/location.rb
@@ -9,12 +9,20 @@ class Location < ActiveRecord::Base
   TIME_FORMAT = '%l:%M %P'
 
   def open? time=Time.now
+    # Set the time's date back to January 1, 2000 so we can do accurate
+    # comparisons with it
+    time = Time.mktime(2000, 1, 1, time.hour, time.min)
+
+    # Figure out if the time is between the hours for the appropriate part of
+    # the week
+    # TODO: Don't add a day to the end Times on the fly, do it when the models
+    # are saved
     if Location.is_weekday? time
       return false unless open_weekdays?
-      (weekday_start..weekday_end).cover? time
+      (weekday_start..(weekday_start < weekday_end ? weekday_end : weekday_end + 1.day)).cover? time
     else
       return false unless open_weekends?
-      (weekend_start..weekend_end).cover? time
+      (weekend_start..(weekend_start < weekend_end ? weekend_end : weekend_end + 1.day)).cover? time
     end
   end
 
