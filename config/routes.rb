8f52acf5b951766f41296686a53decf49bc2e2a5
Nicolas McCurdy <thenickperson@gmail.com>
2013-06-11 21:15:46 -0400

    Use the new syntax for the root route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ OpenAtRit::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  root to: 'locations#index'
+  root 'locations#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
