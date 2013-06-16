6436184d63002acada25315e4765a3a7c70dbd83
Nicolas McCurdy <thenickperson@gmail.com>
2013-06-16 05:09:36 -0400

    Remove Location.reset_date

--- app/models/location.rb
+++ app/models/location.rb
@@ -16,18 +16,12 @@ class Location < ActiveRecord::Base
     presence: true,
     uniqueness: true
 
-  # A callback that runs before any Location is saved. This first resets the
-  # dates of all the Location's Times to 2000/1/1, and then adds a day to end
-  # times if needed to ensure that the end times are always after their
-  # matching start times.
+  # A callback that runs before any Location is saved. This adds a day to end
+  # times if needed to ensure that the end times are always after their matching
+  # start times.
   #
   # TODO refactor
   before_save do
-    Location.reset_date weekday_start
-    Location.reset_date weekday_end
-    Location.reset_date weekend_start
-    Location.reset_date weekend_end
-
     self.weekday_end += 1.day if weekday_end < weekday_start
     self.weekend_end += 1.day if weekend_end < weekend_start
   end
@@ -46,10 +40,6 @@ class Location < ActiveRecord::Base
     # the week
     part_of_week = Location.is_weekday?(time) ? :weekdays : :weekends
     return false unless open_on? part_of_week
-
-    # Set the time's date back to January 1, 2000 so we can do accurate
-    # comparisons with it
-    time = Location.reset_date time
 
     if part_of_week == :weekdays
       start_time = weekday_start
@@ -87,17 +77,6 @@ class Location < ActiveRecord::Base
   end
 
   private
-
-  # Returns a new copy of a given Time with its date reset to 2000/1/1. The
-  # original Time is not modified. This is very useful for making comparisons
-  # between times when you don't care about their specific dates.
-  #
-  # @param [Time] time the original Time, which is used to create the new Time
-  #
-  # @return [Time] a copy of the original Time with its date reset to 2000/1/1
-  def self.reset_date time
-    time.change year: 2000, month: 1, day: 1
-  end
 
   # Returns true if the given Time is on a weekday (Monday-Friday).
   #
