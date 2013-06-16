da44d2043c801e3530cf2fdc956a89276c9add8d
Nicolas McCurdy <thenickperson@gmail.com>
2013-06-16 16:44:39 -0400

    Delete some reset_date calls that accidentally got added in a merge

--- app/models/location.rb
+++ app/models/location.rb
@@ -22,11 +22,6 @@ class Location < ActiveRecord::Base
   #
   # TODO refactor
   before_save do
-    Location.reset_date weekday_start
-    Location.reset_date weekday_end
-    Location.reset_date weekend_start
-    Location.reset_date weekend_end
-
     if weekday_start && weekday_end
       self.weekday_end += 1.day if weekday_end < weekday_start
     end
