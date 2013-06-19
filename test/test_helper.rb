88ab091a86ebe4e49470fa9ef41ce8f4355e2ad7
Nicolas McCurdy <thenickperson@gmail.com>
2013-06-19 16:00:22 -0400

    Bring Zonebie back (fix #19)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,9 @@
 require 'coveralls'
 Coveralls.wear! 'rails'
 
+require 'zonebie'
+Zonebie.set_random_timezone
+
 ENV["RAILS_ENV"] ||= "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
