873918cb7dbc3b392446070071a3fa72662dd0a0
Nicolas McCurdy <thenickperson@gmail.com>
2013-06-18 21:38:27 -0400

    Rename a test method

--- test/models/location_test.rb
+++ test/models/location_test.rb
@@ -9,15 +9,6 @@ class LocationTest < ActiveSupport::TestCase
     @corner_store.save!
   end
 
-  test 'should be initialized properly' do
-    assert_equal 'The Corner Store', @corner_store.name
-    assert_equal Time.utc(2000, 1, 1, 13, 0,  0), @corner_store.weekday_start
-    assert_equal Time.utc(2000, 1, 2, 7,  0,  0), @corner_store.weekday_end
-    assert_equal Time.utc(2000, 1, 1, 15, 30, 0), @corner_store.weekend_start
-    assert_equal Time.utc(2000, 1, 2, 7,  0,  0), @corner_store.weekend_end
-    assert_nil @corner_store.explanation
-  end
-
   #test 'should be sorted by name'
 
   #test 'should validate its name'
@@ -49,6 +40,15 @@ class LocationTest < ActiveSupport::TestCase
 
     assert !Location.is_weekday?(Chronic.parse 'Saturday'), 'Saturday should not be a weekday'
     assert !Location.is_weekday?(Chronic.parse 'Sunday'),   'Sunday should not be a weekday'
+  end
+
+  test 'should have its times adjusted properly' do
+    assert_equal 'The Corner Store', @corner_store.name
+    assert_equal Time.utc(2000, 1, 1, 13, 0,  0), @corner_store.weekday_start
+    assert_equal Time.utc(2000, 1, 2, 7,  0,  0), @corner_store.weekday_end
+    assert_equal Time.utc(2000, 1, 1, 15, 30, 0), @corner_store.weekend_start
+    assert_equal Time.utc(2000, 1, 2, 7,  0,  0), @corner_store.weekend_end
+    assert_nil @corner_store.explanation
   end
 
 end
