c28f4c7895625090d55566d21a5580df87d7cdd7
Nicolas McCurdy <thenickperson@gmail.com>
2013-06-29 14:39:18 -0400

    Add a route for getting a JSON version of the Location index

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ OpenAtRit::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # The home page, which is the list of all Locations.
+  # The home page, which is the list of all Locations
   root 'locations#index'
+
+  # The main page of the JSON API, which also lists all Locations
+  get '/locations.json' => 'locations#index',
+    format: false,
+    defaults: { format: 'json' }
 end
