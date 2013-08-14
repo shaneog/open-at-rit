9db2f6e9e84ce881843efd5000d29c29d6dca295
Nicolas McCurdy <thenickperson@gmail.com>
2013-08-14 17:56:21 -0400

    Append some new (and better) parsing code to the parsing script

--- location_parser.rb
+++ location_parser.rb
@@ -1,8 +1,10 @@
 require 'nokogiri'
 require 'open-uri'
 
+# Old parser
+
 module LocationParser
-  
+
   extend self
 
   @pages = {
@@ -21,3 +23,25 @@ module LocationParser
 end
 
 p LocationParser.locations
+
+# New parser (based off of https://gist.github.com/toroidal-code/6235807)
+
+def next_node_with(current, attribute, cond)
+  while current && current.send(attribute) != cond
+    current = current.next
+  end
+  current
+end
+
+doc = Nokogiri::HTML(open('http://www.rit.edu/fa/diningservices/content/hours-operation'))
+doc.css('h3 a').each do |node|
+  if node.content == "" && node['id'] == node['name']
+    puts node
+    table =  next_node_with(node.parent, :name, "table")
+    rows =  table.search('tr')[1..-1]
+    details = rows.collect do |row|
+      row.search('td//text()').collect{|text|  text.to_s.strip}
+    end
+    puts details
+  end
+end
